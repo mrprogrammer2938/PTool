79597388f82d94fa9beecb972b870892f3be7539
ms.programmer2938 <78996423+msprogrammer2938@users.noreply.github.com>
2021-03-05 11:24:31 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# This code write by (ms.nope)
+echo "     installing..."
+echo ""
+echo "█████████████████████████████"
+echo "█▄─▄▄─█─▄─▄─█─▄▄─█─▄▄─█▄─▄███"
+echo "██─▄▄▄███─███─██─█─██─██─██▀█"
+echo "▀▄▄▄▀▀▀▀▄▄▄▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▄▀"
+echo ""
+chmod +x ptool.py
+echo ""
+echo ""
+cat README.md
