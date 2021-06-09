3324b2bbb528c84a31d8730367e1137d957561e5
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-09 02:31:13 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# This code write by (Mr.nope)
+clear
+echo ""
+echo "Installing..."
+sleep 1
+echo ""
+echo "█████████████████████████████"
+echo "█▄─▄▄─█─▄─▄─█─▄▄─█─▄▄─█▄─▄███"
+echo "██─▄▄▄███─███─██─█─██─██─██▀█"
+echo "▀▄▄▄▀▀▀▀▄▄▄▀▀▄▄▄▄▀▄▄▄▄▀▄▄▄▄▄▀"
+echo ""
+sudo apt install python3
+sudo apt install python
+chmod +x tools
+sleep 1
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+echo "Usage: ./tools"
+echo ""
+exit 1
